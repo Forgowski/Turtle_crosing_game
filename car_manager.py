0dd957caf738badf01f8d4ce3fa5c49cd819df59
Krzysztof Tyszkiewicz <krzysztof.tyszkiewicz.kt@gmail.com>
2022-08-31 17:26:40 +0200

    cars_manager

--- car_manager.py
+++ car_manager.py
@@ -1,9 +1,33 @@
 from turtle import Turtle
+import random
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
 class CarManager(Turtle):
-    #super().__init__()
-    pass
+    def __init__(self):
+        super().__init__()
+        self.cars_speed = STARTING_MOVE_DISTANCE
+        self.cars = []
+        self.hideturtle()
+
+    def create_car(self):
+        new_car = Turtle()
+        new_car.penup()
+        new_car.shape("square")
+        new_car.left(180)
+        new_car.turtlesize(1, 2)
+        new_car.color(random.choice(COLORS))
+        new_car.goto(310, random.randint(-230, 230))
+        self.cars.append(new_car)
+
+    def move_cars(self):
+        for car in self.cars:
+            car.forward(self.cars_speed)
+
+    def increment_speed(self):
+        self.cars_speed += MOVE_INCREMENT
+
+
+
--- main.py
+++ main.py
@@ -6,9 +6,10 @@ from scoreboard import Scoreboard
 
 
 def check_move():
-    screen.onkey(player.go_up, "Up")
+    screen.onkeypress(player.go_up, "Up")
 
 
+frames_counter = 0
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
@@ -16,7 +17,8 @@ screen.tracer(0)
 is_game_on = True
 player = Player()
 screen.listen()
-scoreboard=Scoreboard()
+scoreboard = Scoreboard()
+car_manager = CarManager()
 
 while is_game_on:
     time.sleep(0.1)
@@ -24,6 +26,13 @@ while is_game_on:
     if player.is_finish():
         scoreboard.level += 1
         scoreboard.update_level()
+        car_manager.increment_speed()
+
+    if frames_counter % 3 == 0:
+        car_manager.create_car()
+
+    car_manager.move_cars()
     screen.update()
+    frames_counter += 1
 
 screen.exitonclick()
